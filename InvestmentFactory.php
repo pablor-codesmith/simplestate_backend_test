e39794bf99193a09154d18db96fdc4f58e1820ef
pablo.ruhl <pabloalejandroruhl@gmail.com>
2024-04-06 20:09:50 -0300

    Fix

--- InvestmentFactory.php
+++ InvestmentFactory.php
@@ -5,6 +5,7 @@ namespace SimpleState\Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use SimpleState\Models\Project;
+use SimpleState\Models\Transaction;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Investment>
@@ -23,7 +24,8 @@ class InvestmentFactory extends Factory
             'status' => fake()->randomElement(['pending','finished','approved']),
             'user_id' => User::factory(),
             'project_id' => Project::factory(),
-            'created_at' => now()->format('Y-m-d')
+            'created_at' => now()->format('Y-m-d'),
+            'transaction_id' => Transaction::factory(),
         ];
     }
 }
