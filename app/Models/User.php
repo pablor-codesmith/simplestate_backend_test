ee87e087859ad49891cbcd80f1f5d2fa47f7df77
pablo.ruhl <pabloalejandroruhl@gmail.com>
2024-04-06 18:46:26 -0300

    Add Fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'gender',
+        'social_security_number',
         'email',
         'password',
     ];
